3f58f91e8f5ebb176a1210cb5e95459e7b0d4399
Yeuk Hon Wong <yeukhon@mozilla.com>
2013-06-10 13:17:20 -0700

    Fixed #36. Fixed #11. RobotsPlugins now use robots-scanner validator.
    The robots-scanner has a very simple scanner to scan the content of
    robots.txt. The overall validation in RobotsPlugins is written in the
    commit (see plugins/basics.py).

--- tests/functional/plugins/test_robot.py
+++ tests/functional/plugins/test_robot.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from flask import Flask, make_response
+from flask import Flask, make_response, redirect, url_for
 from multiprocessing import Process
 
 from base import TestPluginBaseClass, test_app, _kill_ports
