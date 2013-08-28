a6c5781d432947293dd1e9858a5d26640212210b
Stefan Arentz <stefan@arentz.ca>
2013-08-28 13:21:52 -0400

    Fixes #233 Test failures in functional/views/test_groups.py

--- tests/functional/views/test_groups.py
+++ tests/functional/views/test_groups.py
@@ -75,23 +75,31 @@ class TestGroupAPIs(TestAPIBaseClass):
 
     # issue #215
     def test_retrieve_issue_status_and_issues_by_group(self):
-        """ Don't be shock. This test fits here; by querying
-        user and group, we should only be given the lastest
-        issue status. """
+        # Don't be shock. This test fits here; by querying
+        # user and group, we should only be given the latest
+        # issue status.
 
         res = self.create_user()
         res1 = self.create_group(group_name="test1", users=[self.email])
         res2 = self.create_group(group_name="test2", users=[self.email])
-        res3 = self.create_site(groups=["test1"], site="http://foo.com")
-        res4 = self.create_site(groups=["test2"], site="http://bar.com")
+        plan = { "name": "test",
+                 "description": "Test",
+                 "workflow": [ { "plugin_name": "minion.plugins.basic.AlivePlugin",
+                                 "description": "Test if the site is alive",
+                                 "configuration": { "foo": "bar" }
+                                 } ] }
+        self.create_plan(plan)
+        res3 = self.create_site(groups=["test1"], site="http://foo.com", plans=['test'])
+        res4 = self.create_site(groups=["test2"], site="http://bar.com", plans=['test'])
         # if we query just test1, should get back only foo.com
-        res5 = self.get_reports_status(user=self.email, group="test1")
+        res5 = self.get_reports_status(user=self.email, group_name="test1")
         r = res5.json()['report']
+        print r
         self.assertEqual(len(r), 1) # there should just be one dict returned in the list
         self.assertEqual(r[0]['target'], "http://foo.com")
 
         # if we try it on get_report_issues we should see similar result
-        res6 = self.get_report_issues(user=self.email, group="test1")
+        res6 = self.get_reports_issues(user=self.email, group_name="test1")
         r = res6.json()['report']
         self.assertEqual(len(r), 1) # there should just be one dict returned in the list
         self.assertEqual(r[0]['target'], "http://foo.com")
