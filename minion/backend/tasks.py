a5c3bde8c4b5bf10f3f0ddd280afe88d4c3dd7b8
Stefan Arentz <stefan@arentz.ca>
2013-08-30 13:25:04 -0400

    Fixes #248 Exceptions thrown in tasks.scan() do not result in a failed scan

--- minion/backend/tasks.py
+++ minion/backend/tasks.py
@@ -58,7 +58,7 @@ def scan_start(scan_id, t):
                            "started": datetime.datetime.utcfromtimestamp(t)}})
 
 @celery.task
-def scan_finish(scan_id, state, t):
+def scan_finish(scan_id, state, t, failure=None):
 
     try:
 
@@ -75,9 +75,15 @@ def scan_finish(scan_id, state, t):
         # Mark the scan as finished with the provided state
         #
 
-        scans.update({"id": scan_id},
-                     {"$set": {"state": state,
-                               "finished": datetime.datetime.utcfromtimestamp(t)}})
+        if failure:
+            scans.update({"id": scan_id},
+                         {"$set": {"state": state,
+                                   "finished": datetime.datetime.utcfromtimestamp(t),
+                                   "failure": failure}})
+        else:
+            scans.update({"id": scan_id},
+                         {"$set": {"state": state,
+                                   "finished": datetime.datetime.utcfromtimestamp(t)}})
 
         #
         # If there are remaining plugin sessions that are still in the CREATED state
@@ -512,123 +518,145 @@ def queue_for_session(session, cfg):
 @celery.task(ignore_result=True)
 def scan(scan_id):
 
-    #
-    # See if the scan exists.
-    #
-
-    scan = get_scan(cfg['api']['url'], scan_id)
-    if not scan:
-        logger.error("Cannot load scan %s" % scan_id)
-        return
-
-    #
-    # Is the scan in the right state to be started?
-    #
-
-    if scan['state'] != 'QUEUED':
-        logger.error("Scan %s has invalid state. Expected QUEUED but got %s" % (scan_id, scan['state']))
-        return
-
-    #
-    # Move the scan to the STARTED state
-    #
-
-    scan['state'] = 'STARTED'
-    send_task("minion.backend.tasks.scan_start",
-              [scan_id, time.time()],
-              queue='state').get()
-
-    #
-    # Check this site against the access control lists
-    #
-
-    if not scannable(scan['configuration']['target'],
-                     scan_config().get('whitelist', []),
-                     scan_config().get('blacklist', [])):
-        return set_finished(scan_id, 'ABORTED')
-
-    #
-    # Verify ownership prior to running scan
-    #
-
     try:
-        target = scan['configuration']['target']
-        site = get_site_info(cfg['api']['url'], target)
-        if not site:
-            return set_finished(scan_id, 'ABORTED')
-        if site.get('verification') and site['verification']['enabled']:
-            verified = ownership.verify(target, site['verification']['value'])
-            if not verified:
-                return set_finished(scan_id, 'ABORTED')
-    except ownership.OwnerVerifyError:
-        return set_finished(scan_id, 'ABORTED')
-
-    #
-    # Run each plugin session
-    #
-
-    for session in scan['sessions']:
 
         #
-        # Mark the session as QUEUED
+        # See if the scan exists.
         #
 
-        session['state'] = 'QUEUED'
-        #scans.update({"id": scan['id'], "sessions.id": session['id']}, {"$set": {"sessions.$.state": "QUEUED", "sessions.$.queued": datetime.datetime.utcnow()}})
-        send_task("minion.backend.tasks.session_queue",
-                  [scan['id'], session['id'], time.time()],
-                  queue='state').get()
-
-        #
-        # Execute the plugin. The plugin worker will set the session state and issues.
-        #
-
-        logger.info("Scan %s running plugin %s" % (scan['id'], session['plugin']['class']))
-
-        queue = queue_for_session(session, cfg)
-        result = send_task("minion.backend.tasks.run_plugin",
-                           [scan_id, session['id']],
-                           queue=queue)
-
-        #scans.update({"id": scan_id, "sessions.id": session['id']}, {"$set": {"sessions.$._task": result.id}})
-        send_task("minion.backend.tasks.session_set_task_id",
-                  [scan_id, session['id'], result.id],
-                  queue='state').get()
-
-        try:
-            plugin_result = result.get()
-        except TaskRevokedError as e:
-            plugin_result = "STOPPED"
-
-        session['state'] = plugin_result
-
-        #
-        # If the user stopped the workflow or if the plugin aborted then stop the whole scan
-        #
-
-        if plugin_result in ('ABORTED', 'STOPPED'):
-            # Mark the scan as failed
-            #scans.update({"id": scan_id}, {"$set": {"state": plugin_result, "finished": datetime.datetime.utcnow()}})
-            send_task("minion.backend.tasks.scan_finish",
-                      [scan_id, plugin_result, time.time()],
-                      queue='state').get()
-            # Mark all remaining sessions as cancelled
-            for s in scan['sessions']:
-                if s['state'] == 'CREATED':
-                    s['state'] = 'CANCELLED'
-                    #scans.update({"id": scan['id'], "sessions.id": s['id']}, {"$set": {"sessions.$.state": "CANCELLED", "sessions.$.finished": datetime.datetime.utcnow()}})
-                    send_task("minion.backend.tasks.session_finish",
-                              [scan['id'], s['id'], "CANCELLED", time.time()],
-                              queue='state').get()
-            # We are done with this scan
+        scan = get_scan(cfg['api']['url'], scan_id)
+        if not scan:
+            logger.error("Cannot load scan %s" % scan_id)
             return
 
-    #
-    # Move the scan to the FINISHED state
-    #
+        #
+        # Is the scan in the right state to be started?
+        #
 
-    scan['state'] = 'FINISHED'
-    #scans.update({"id": scan_id}, {"$set": {"state": "FINISHED", "finished": datetime.datetime.utcnow()}})
-    send_task("minion.backend.tasks.scan_finish",
-              [scan_id, "FINISHED", time.time()],
-              queue='state').get()
+        if scan['state'] != 'QUEUED':
+            logger.error("Scan %s has invalid state. Expected QUEUED but got %s" % (scan_id, scan['state']))
+            return
+
+        #
+        # Move the scan to the STARTED state
+        #
+
+        scan['state'] = 'STARTED'
+        send_task("minion.backend.tasks.scan_start",
+                  [scan_id, time.time()],
+                  queue='state').get()
+
+        #
+        # Check this site against the access control lists
+        #
+
+        if not scannable(scan['configuration']['target'],
+                         scan_config().get('whitelist', []),
+                         scan_config().get('blacklist', [])):
+            return set_finished(scan_id, 'ABORTED')
+
+        #
+        # Verify ownership prior to running scan
+        #
+
+        try:
+            target = scan['configuration']['target']
+            site = get_site_info(cfg['api']['url'], target)
+            if not site:
+                return set_finished(scan_id, 'ABORTED')
+            if site.get('verification') and site['verification']['enabled']:
+                verified = ownership.verify(target, site['verification']['value'])
+                if not verified:
+                    return set_finished(scan_id, 'ABORTED')
+        except ownership.OwnerVerifyError:
+            return set_finished(scan_id, 'ABORTED')
+
+        #
+        # Run each plugin session
+        #
+
+        for session in scan['sessions']:
+
+            #
+            # Mark the session as QUEUED
+            #
+
+            session['state'] = 'QUEUED'
+            #scans.update({"id": scan['id'], "sessions.id": session['id']}, {"$set": {"sessions.$.state": "QUEUED", "sessions.$.queued": datetime.datetime.utcnow()}})
+            send_task("minion.backend.tasks.session_queue",
+                      [scan['id'], session['id'], time.time()],
+                      queue='state').get()
+
+            #
+            # Execute the plugin. The plugin worker will set the session state and issues.
+            #
+
+            logger.info("Scan %s running plugin %s" % (scan['id'], session['plugin']['class']))
+
+            queue = queue_for_session(session, cfg)
+            result = send_task("minion.backend.tasks.run_plugin",
+                               [scan_id, session['id']],
+                               queue=queue)
+
+            #scans.update({"id": scan_id, "sessions.id": session['id']}, {"$set": {"sessions.$._task": result.id}})
+            send_task("minion.backend.tasks.session_set_task_id",
+                      [scan_id, session['id'], result.id],
+                      queue='state').get()
+
+            try:
+                plugin_result = result.get()
+            except TaskRevokedError as e:
+                plugin_result = "STOPPED"
+
+            session['state'] = plugin_result
+
+            #
+            # If the user stopped the workflow or if the plugin aborted then stop the whole scan
+            #
+
+            if plugin_result in ('ABORTED', 'STOPPED'):
+                # Mark the scan as failed
+                #scans.update({"id": scan_id}, {"$set": {"state": plugin_result, "finished": datetime.datetime.utcnow()}})
+                send_task("minion.backend.tasks.scan_finish",
+                          [scan_id, plugin_result, time.time()],
+                          queue='state').get()
+                # Mark all remaining sessions as cancelled
+                for s in scan['sessions']:
+                    if s['state'] == 'CREATED':
+                        s['state'] = 'CANCELLED'
+                        #scans.update({"id": scan['id'], "sessions.id": s['id']}, {"$set": {"sessions.$.state": "CANCELLED", "sessions.$.finished": datetime.datetime.utcnow()}})
+                        send_task("minion.backend.tasks.session_finish",
+                                  [scan['id'], s['id'], "CANCELLED", time.time()],
+                                  queue='state').get()
+                # We are done with this scan
+                return
+
+        #
+        # Move the scan to the FINISHED state
+        #
+
+        scan['state'] = 'FINISHED'
+        #scans.update({"id": scan_id}, {"$set": {"state": "FINISHED", "finished": datetime.datetime.utcnow()}})
+        send_task("minion.backend.tasks.scan_finish",
+                  [scan_id, "FINISHED", time.time()],
+                  queue='state').get()
+
+    except Exception as e:
+
+        #
+        # Our exception strategy is simple: if anything was thrown above that we did not explicitly catch then
+        # we assume there was a non recoverable error that made the scan fail. We mark it as such and
+        # record the exception.
+        #
+
+        logger.exception("Error while running scan. Marking scan FAILED.")
+
+        try:
+            failure = { "hostname": socket.gethostname(),
+                        "message": str(e),
+                        "exception": traceback.format_exc() }
+            send_task("minion.backend.tasks.scan_finish",
+                      [scan_id, "FAILED", time.time(), failure],
+                      queue='state').get()
+        except Exception as e:
+            logger.exception("Error when marking scan as FAILED")
