55b73776b4530ff0e860d0123b958a3fa06ef21d
Yeuk Hon Wong <yeukhon@acm.org>
2014-01-13 18:28:18 +0000

    Fixed #279. Rewrite CSPPlugin.
    What has changed?
    
    + tests are completly rewritten.
    + CSPPlugin is completely rewritten to support 1.0 syntax.
    + Compare to the previous CSSPlugin, CSP and XCSP presence check is always required.
    
    The orginal rewrite expected more issues to raise, but I have given up that idea, Instead,
    only relevant and basic syntax checks are done. Issues/summaries like how many directives
    inherit from default-src are not available (although they were implemented I backed out those
    codes during squash). Also, this plugin currently only checks the syntax of the CSP header; so
    XCSP, CSP-RO and XCSP-RO are not verified. An experimental CSPPlugin (but separate) should
    be developed. Until then this plugin can remain simple like this.

--- tests/functional/plugins/test_csp.py
+++ tests/functional/plugins/test_csp.py
@@ -7,194 +7,247 @@ import sys
 import time
 import unittest
 
-from flask import make_response
+from flask import make_response, request
+from collections import namedtuple
 
 from base import TestPluginBaseClass, test_app
+from minion.plugins.basic import CSPPlugin
 
-def _make_res(type, value):
+@test_app.route('/test')
+def endpoint():
+    headers = request.args.getlist("headers")
+    policy = request.args.get("policy", "default-src 'self';")
     res = make_response("")
-    if type.lower() == 'x':
-        res.headers['X-Content-Security-Policy'] = value
-    elif type.lower() == 'c':
-        res.headers['Content-Security-Policy'] = value
+
+    for h in headers:
+        _h = h.lower()
+        if _h == 'xcsp':
+            res.headers.add('X-Content-Security-Policy', policy)
+        elif _h == 'csp':
+            res.headers.add('Content-Security-Policy', policy)
+        elif _h == 'csp-ro':
+            res.headers.add('Content-Security-Policy-Report-Only', policy)
+        elif _h == "xcsp-ro":
+            res.headers['X-Content-Security-Policy-Report-Only'] = policy
     return res
-
-RULES = {'DEFAULT-SRC': "default-src 'self'",
-         "DEFAULT-SRC-DOMAIN": "default-src 'self' mydomain.com",
-         "DEFAULT-SRC-DOMAIN-SUB-DOMAIN": "default-src 'self' *.mydomain.com",
-         "DEFAULT-SRC-DOMAIN-SUB-DOMAIN-IMG-MEDIA-SCRIPT": 
-            "default-src 'self'; img-src *; media-src media1.com media2.com; script-src userscripts.example.com",
-         "EVAL": "script-src 'self' 'unsafe-eval' https://mydomain.com",
-         "INLINE": "script-src 'self' 'unsafe-inline' https://mydomain.com",
-         "BLOB": "default-src 'self' *.mega.co.nz http://*.mega.co.nz;" + 
-                 "script-src 'self' mega.co.nz data: blob:;",
-         "MALFORMED": "default-src 'selff'",
-}
-
-@test_app.route('/no-csp')
-def no_csp():
-    res = make_response()
-    return res
-
-@test_app.route('/default-src-self/<type>')
-def default_src_self(type):
-    return _make_res(type, RULES['DEFAULT-SRC'])
-
-@test_app.route('/default-src-self-trusted-domain/<type>')
-def default_src_self_trusted_domain(type):
-    return _make_res(type, RULES['DEFAULT-SRC-DOMAIN'])
-
-@test_app.route('/default-src-self-trusted-domain-subdomain/<type>')
-def default_src_self_trusted_domain_subdomain(type):
-    return _make_res(type, RULES['DEFAULT-SRC-DOMAIN-SUB-DOMAIN'])
-
-@test_app.route('/default-src-self-trusted-subdomain-all-img-some-media-some-script/<type>')
-def default_src_self_trusted_subdomain_all_img_some_media_some_script(type):
-    return _make_res(type, RULES['DEFAULT-SRC-DOMAIN-SUB-DOMAIN-IMG-MEDIA-SCRIPT'])
-
-@test_app.route('/malformed-csp')
-def malformed_csp():
-    return _make_res('c', RULES['MALFORMED'])
-
-@test_app.route('/eval-csp')
-def eval_csp():
-    return _make_res('x', RULES['EVAL'])
-
-@test_app.route('/inline-csp')
-def inline_csp():
-    return _make_res('x', RULES['INLINE'])
-
-@test_app.route('/dual-policy')
-def daul_policy():
-    resp = make_response('')
-    resp.headers['Content-Security-Policy'] = 'default-src *;'
-    resp.headers['Content-Security-Policy-Report-Only'] = 'default *;'
-    return resp
-
-@test_app.route('/report-only')
-def report_only():
-    resp = make_response('')
-    resp.headers['Content-Security-Policy-Report-Only'] = 'default *;'
-    return resp
-
-@test_app.route('/blob')
-def blog():
-    return _make_res('c', RULES['BLOB'])
-
+    
 class TestCSPPlugin(TestPluginBaseClass):
     __test__ = True
+    Issue = namedtuple('Issue', 'code summary severity')
+    CSP = CSPPlugin()
+
     @classmethod
     def setUpClass(cls):
         super(TestCSPPlugin, cls).setUpClass()
         cls.pname = 'CSPPlugin'
 
-    def validate_csp(self, runner_resp, request_resp, expected=None, expectation=True):
-        if expectation is True:
-            self.assertEqual('Info', runner_resp[1]['data']['Severity'])
-            self.assertEqual('Content-Security-Policy header set properly', runner_resp[1]['data']['Summary'])
-            self.assertEqual("The Content-Security-Policy header is set properly. Neither 'unsafe-inline' or \
-'unsafe-eval' is enabled.", runner_resp[1]['data']['Description'])
-            self.assertEqual('FINISHED', runner_resp[2]['data']['state'])
+    def _get_summary(self, key, fill_with=None):
+        _summary = self.CSP.REPORTS[key]['Summary']
+        if fill_with:
+            return _summary.format(**fill_with)
+        else:
+            return _summary
 
-        elif expectation == 'BOTH-SET':
-            self.assertEqual('Content-Security-Policy-Report-Only and Content-Security-Policy are set',
-                    runner_resp[1]['data']['Summary'])
-            self.assertEqual('High', runner_resp[1]['data']['Severity'])
+    def _run(self, headers=None, policy=None):
+        API = "http://localhost:1234/test"
+        r = requests.Request('GET', API, 
+            params={"headers": headers, "policy": policy}).prepare()
+        runner_resp = self.run_plugin(self.pname, r.url)
+        return runner_resp
 
-        elif expectation == 'REPORT-ONLY':
-            self.assertEqial('Content-Security-Policy-Report-Only does not enforce any CSP policy. Use \
-Content-Security-Policy to secure your site.', runner_resp[1]['data']['Description'])                    
-            self.assertEqual('Content-Security-Policy-Report-Only header set',
-                    runner_resp[1]['data']['Summary'])
-            self.assertEqual('High', runner_resp[1]['data']['Severity'])
+    def _get_issues(self, resps):
+        issues = []
+        for issue in resps:
+            if issue.get('data') and issue['data'].get('Code'):
+                _issue = self.Issue(issue['data']['Code'],
+                                    issue['data']['Summary'],
+                                    issue['data']['Severity'])
+                issues.append(_issue)
+        return issues
 
-        elif expectation is False:
-            self.assertEqual('No Content-Security-Policy header set',
-                    runner_resp[1]['data']['Summary'])
-            self.assertEqual('High', runner_resp[1]['data']['Severity'])
+    def _test_expecting_codes(self, issues, expects, message):
+        self.assertEqual(len(issues), len(expects), msg=message)
+        for expect in expects:
+            self._test_expecting_code(issues, expect, message)
 
-        elif expectation == 'INVALID':
-            self.assertEqual('High', runner_resp[1]['data']['Severity'])
-            self.assertEqual('Malformed Content-Security-Policy header is set', \
-                runner_resp[1]['data']['Summary'])
-            self.assertEqual('Malformed CSP header set: {value} does not seem like a valid source expression for {name}'.format(
-                value=expected['value'], name=expected['name']), runner_resp[1]['data']['Description'])
+    def _test_expecting_code(self, issues, expect, message):
+        codes = [issue.code for issue in issues]
+        self.assertEqual(True, expect in codes, msg=message)
+    
+    def _test_expecting_summary(self, issues, summary_name, message,
+            fill_with=None):
+        summaries = [issue.summary for issue in issues]
+        expecting_summary = self._get_summary(summary_name, fill_with=fill_with)
+        self.assertEqual(True, expecting_summary in summaries, msg=message)
 
-        elif expectation == 'UNSAFE-INLINE':
-            self.assertEqual('High', runner_resp[1]['data']['Severity'])
-            self.assertEqual("'unsafe-inline' is set in Content-Security-Policy header", \
-                runner_resp[1]['data']['Summary'])
+    # Start testing
+    # Testing pattern:
+    # 1. first assert the main issue is raised for that test
+    # 2. then assert other expecting issues
+    #
+    # NOTE: Good report should include {msg: start} and finish
+    #       which adds 2 to the total number of expected
+    #       issues returned in the response.
 
-        elif expectation == 'UNSAFE-EVAL':
-            self.assertEqual('High', runner_resp[1]['data']['Severity'])
-            self.assertEqual("'unsafe-eval' is set in Content-Security-Policy header", \
-                runner_resp[1]['data']['Summary'])
+    def test_csp(self):
+        resp = self._run(headers=['csp'])
+        issues = self._get_issues(resp)
+        self._test_expecting_summary(
+            issues,
+            'csp-set',
+            "CSP is set should be in issues")
 
-        elif expectation is False:
-            self.assertEqual('High', runner_resp[1]['data']['Severity'])
-            self.assertEqual('No X-Content-Security-Policy header set"',
-                    runner_resp[1]['data']['Summary'])
-        """
-        elif expectation == 'EVAL-ENABLED':
-            self.assertEqual('CSP Rules allow unsafe-eval',
-                    runner_resp[1]['data']['Summary'])
-            self.assertEqual('High', runner_resp[1]['data']['Severity'])
+        self._test_expecting_codes(issues,
+            ['CSP-1', 'CSP-5'],
+            "Expecting CSP is set, XCSP is not set and number of unspecified directives")
 
-        elif expectation == 'INLINE-ENABLED':
-            self.assertEqual('CSP Rules allow unsafe-inline',
-                    runner_resp[1]['data']['Summary'])
-            self.assertEqual('High', runner_resp[1]['data']['Severity'])
-        """  
+    def test_no_csp_and_no_xcsp(self):
+        resp = self._run()
+        issues = self._get_issues(resp)
+        self._test_expecting_summary(
+            issues,
+            'csp-not-set',
+            "CSP is not set should be in issues")
 
-    def test_no_csp(self):
-        api_name = '/no-csp'
-        self.validate_plugin(api_name, self.validate_csp, expectation=False)
+        self._test_expecting_codes(
+            issues,
+            ['CSP-2', 'CSP-5'],
+            "CSP is not set and XCSP is not set")
 
-    def test_x_default_src_self(self):
-        api_name= '/default-src-self/x'
-        self.validate_plugin(api_name, self.validate_csp, expectation=True)
-    def test_x_default_src_self_trusted_domain(self):
-        api_name = '/default-src-self-trusted-domain/x'
-        self.validate_plugin(api_name, self.validate_csp, expectation=True)
-    def test_x_default_src_self_trusted_domain_subdomain(self):
-        api_name = '/default-src-self-trusted-domain-subdomain/x'
-        self.validate_plugin(api_name, self.validate_csp, expectation=True)
-    def test_x_default_src_self_trusted_domain_subdomain_all_img_some_media_some_script(self):
-        api_name = '/default-src-self-trusted-subdomain-all-img-some-media-some-script/x'
-        self.validate_plugin(api_name, self.validate_csp, expectation=True)
+    def test_only_csp_ro_only(self):
+        resp = self._run(headers=['csp-ro'])
+        issues = self._get_issues(resp)
 
-    def test_default_src_self(self):
-        api_name= '/default-src-self/c'
-        self.validate_plugin(api_name, self.validate_csp, expectation=True)
-    def test_default_src_self_trusted_domain(self):
-        api_name = '/default-src-self-trusted-domain/c'
-        self.validate_plugin(api_name, self.validate_csp, expectation=True)
-    def test_default_src_self_trusted_domain_subdomain(self):
-        api_name = '/default-src-self-trusted-domain-subdomain/c'
-        self.validate_plugin(api_name, self.validate_csp, expectation=True)
-    def test_default_src_self_trusted_domain_subdomain_all_img_some_media_some_script(self):
-        api_name = '/default-src-self-trusted-subdomain-all-img-some-media-some-script/c'
-        self.validate_plugin(api_name, self.validate_csp, expectation=True)
+        self._test_expecting_summary(
+            issues,
+            'csp-ro-only-set',
+            "CSP-Report-Only is set should be in issues")
 
-    def test_malformed_csp(self):
-        api_name = '/malformed-csp'
-        self.validate_plugin(api_name, self.validate_csp, expectation='INVALID',
-                expected={'value': "'selff'", 'name': 'default-src', 'hname': 'content-security-policy'})
-    def test_eval_csp(self):
-        api_name = '/eval-csp'
-        self.validate_plugin(api_name, self.validate_csp, expectation='UNSAFE-EVAL')
-    def test_inline_csp(self):
-        api_name = '/inline-csp'
-        self.validate_plugin(api_name, self.validate_csp, expectation='UNSAFE-INLINE')
+        self._test_expecting_codes(
+            issues,
+            ['CSP-2', 'CSP-3', 'CSP-5'],
+            "Expecting CSP is not set, CSP-RO is set and XCSP is not set")
 
-    def test_dual_policy(self):
-        api_name = '/dual-policy'
-        self.validate_plugin(api_name, self.validate_csp, expectation='BOTH-SET')
+    def test_only_xcsp_is_set(self):
+        resp = self._run(headers=['xcsp'])
+        issues = self._get_issues(resp)
+        self._test_expecting_summary(
+            issues,
+            'xcsp-set',
+            "XCSP is set should be in issues")
 
-    def test_report_only(self):
-        api_name = '/report_only'
-        self.validate_plugin(api_name, self.validate_csp, expectation='REPORT_ONLY')
+        self._test_expecting_codes(
+            issues,
+            ['CSP-2', 'CSP-4'],
+            "Expecting CSP is not set and XCSP is set.")
 
-    def test_blob(self):
-        api_name = '/blob'
-        self.validate_plugin(api_name, self.validate_csp, expectation=True)
+    def test_only_xcsp_ro_only(self):
+        resp = self._run(headers=['xcsp-ro'])
+        issues = self._get_issues(resp)
+        self._test_expecting_summary(
+            issues,
+            'xcsp-ro-only-set',
+            "XCSP-Report-Only is set should be in issues")
+
+        self._test_expecting_codes(
+            issues,
+            ['CSP-2', 'CSP-5', 'CSP-6'],
+            "Expecting CSP and XCSP are not set but XCSP-RO is set")
+
+    def test_csp_and_csp_ro_are_set(self):
+        resp = self._run(headers=['csp', 'csp-ro'])
+        issues = self._get_issues(resp)
+        self._test_expecting_summary(
+            issues,
+            'csp-csp-ro-set',
+            "Both CSP and CSP-Report-Only are set should be in issues")
+
+        self._test_expecting_codes(
+            issues,
+            ['CSP-1', 'CSP-5', 'CSP-7'],
+            "Expecting CSP, CSP and CSP-RO are set but XCSP is not set")
+
+    def test_xcsp_and_xcsp_ro_are_set(self):
+        resp = self._run(headers=['xcsp', 'xcsp-ro'])
+        issues = self._get_issues(resp)
+        self._test_expecting_summary(
+            issues,
+            'xcsp-xcsp-ro-set',
+            "Both XCSP and XCSP-Report-Only are set should be in issues")
+
+        self._test_expecting_codes(
+            issues,
+            ['CSP-2', 'CSP-4', 'CSP-8'],
+            "Expecting XCSP, XCSP and XCSP-RO are set but CSP is not set")
+
+    def test_unknown_directive_in_csp_only_header(self):
+        resp = self._run(headers=['csp'],
+            policy="default-src 'self'; unknown-directive 'self';")
+        issues = self._get_issues(resp)
+        self._test_expecting_summary(
+            issues,
+            'unknown-directive',
+            "1 unknown directive should be in issues",
+            fill_with={"count": 1})
+
+        self._test_expecting_codes(
+            issues,
+            ['CSP-1', 'CSP-5', 'CSP-9'],
+            "Expecting CSP is set, XCSP is not set and 1 unknown directive")
+
+    def test_csp_deprecated_directive(self):
+        resp = self._run(headers=['csp'],
+            policy="allow 'self'; xhr-src foobar.com;")
+        issues = self._get_issues(resp)
+        self._test_expecting_summary(
+            issues,
+            'deprecated-directive',
+            "2 unknown directive should be in issues",
+            fill_with={"count": 2})
+
+        self._test_expecting_codes(
+            issues,
+            ['CSP-1', 'CSP-5', 'CSP-10'],
+            "Expecting CSP is set, XCSP is not set, 2 deprecated directives.")
+
+    def test_none_used_with_other_source_expressions(self):
+        resp = self._run(headers=['csp'],
+            policy="default-src 'self'; style-src 'self' 'none';")
+        issues = self._get_issues(resp)
+        self._test_expecting_summary(
+            issues,
+            'bad-none',
+            "'none' issue should be in issue")
+
+        self._test_expecting_codes(
+            issues,
+            ['CSP-1', 'CSP-5', 'CSP-11'],
+            "Expecting CSP is set, XCSP is not set, and improper use of 'none'")
+
+    def test_inline(self):
+        resp = self._run(headers=['csp'],
+            policy="default-src 'self'; style-src 'unsafe-inline';")
+        issues = self._get_issues(resp)
+        self._test_expecting_summary(
+            issues,
+            'inline',
+            "unsafe-inline is enabled should be in issue")
+
+        self._test_expecting_codes(
+            issues,
+            ['CSP-1', 'CSP-5', 'CSP-12'],
+            "Expecting CSP is set, XCSP is not set, and unsafe-inline is enabled")
+
+    def test_eval(self):
+        resp = self._run(headers=['csp'],
+            policy="default-src 'self'; script-src 'unsafe-eval';")
+        issues = self._get_issues(resp)
+        self._test_expecting_summary(
+            issues,
+            'eval',
+            "unsafe-eval is enabled should be in issue")
+
+        self._test_expecting_codes(
+            issues,
+            ['CSP-1', 'CSP-5', 'CSP-13'],
+            "Expecting CSP is set, XCSP is not set, and unsafe-eval is enabled")
