c02d594a2ca5fc9436d0911cd46fa4d394140750
Yeuk Hon Wong <yeukhon@acm.org>
2014-01-20 20:53:59 +0000

    Working on #284. Updated test_alive.py
    + Update test_alive.py with the new test driver.
    
    We will add the new driver to the base test class once all tests have been converted.
    
    + timeout is commented out
    
    This is because minion.curly does not handle code 28 (operation timeout error).
    This will be a separate issue to handle.

--- tests/functional/plugins/test_alive.py
+++ tests/functional/plugins/test_alive.py
@@ -7,26 +7,71 @@ import sys
 import time
 import unittest
 
-from flask import make_response
-
+from collections import namedtuple
+from flask import make_response, request
 from base import TestPluginBaseClass, test_app
+from minion.plugins.basic import AlivePlugin
 
-@test_app.route('/alive')
-def alive_alive():
-    res = make_response("<h1>HELLO WORLD!</h1>")
-    return res
+@test_app.route('/test')
+def endpoint():
+    timeout = request.args.get("timeout")
+    res = make_response("")
 
-@test_app.route('/timeout')
-def alive_timeout():
-    res = make_response('<h1> TIMEOUT </h1>')
-    time.sleep(7)
+    if timeout:
+        time.sleep(5)
+        return res
+    else:
+        return res
 
 class TestAlivePlugin(TestPluginBaseClass):
     __test__ = True
+    Alive = AlivePlugin()
+    Issue = namedtuple('Issue', 'code summary severity')
+
     @classmethod
     def setUpClass(cls):
         super(TestAlivePlugin, cls).setUpClass()
         cls.pname = 'AlivePlugin'
+
+    def _get_summary(self, key, fill_with=None):
+        _summary = self.Alive.REPORTS[key]['Summary']
+        if fill_with:
+            return _summary.format(**fill_with)
+        else:
+            return _summary
+
+    def _run(self, endpoint="/test", headers=None, timeout=None):
+        API = "http://localhost:1234" + endpoint
+        r = requests.Request('GET', API,
+            params={"timeout": timeout}).prepare()
+        runner_resp = self.run_plugin(self.pname, r.url)
+        return runner_resp
+
+    def _get_issues(self, resps):
+        issues = []
+        for issue in resps:
+            if issue.get('data') and issue['data'].get('Code'):
+                _issue = self.Issue(issue['data']['Code'],
+                                    issue['data']['Summary'],
+                                    issue['data']['Severity'])
+                issues.append(_issue)
+        return issues
+
+    def _test_expecting_codes(self, issues, expects, message):
+        self.assertEqual(len(issues), len(expects), msg=message)
+        for expect in expects:
+            self._test_expecting_code(issues, expect, message)
+
+    def _test_expecting_code(self, issues, expect, message):
+        codes = [issue.code for issue in issues]
+        self.assertEqual(True, expect in codes, msg=message)
+
+    def _test_expecting_summary(self, issues, summary_name, message,
+            fill_with=None):
+        summaries = [issue.summary for issue in issues]
+        expecting_summary = self._get_summary(summary_name, fill_with=fill_with)
+        self.assertEqual(True, expecting_summary in summaries, msg=message)
+
 
     def validate_alive(self, runner_resp, request_resp, expected=None, expectation=True):
         if expectation is True:
@@ -49,14 +94,43 @@ class TestAlivePlugin(TestPluginBaseClass):
                 # requests will throw exeception if not connectable
                 self.assertEqual(requests.exceptions.ConnectionError, type(request_resp))
 
-    def test_alive_200(self):
-        api_name = '/alive'
-        self.validate_plugin(api_name, self.validate_alive, expectation=True)
-    def test_alive_404(self):
-        api_name = '/not-alive'
-        self.validate_plugin(api_name, self.validate_alive, expectation='404')
-    def test_alive_timeout(self):
-        api_name = '/timeout'
-        self.validate_plugin(api_name, self.validate_alive, \
-            expected='http://localhost:1234/timeout', expectation='TIMEOUT')
+    def test_alive_200_return_reachable(self):
+        resp = self._run()
+        issues = self._get_issues(resp)
+        self._test_expecting_summary(
+            issues,
+            "good",
+            "Site is alive should be in issues")
 
+        self._test_expecting_codes(
+            issues,
+            ["ALIVE-0"],
+            "Expecting to see site is alive.")
+
+    def test_alive_404_return_not_reachable(self):
+        resp = self._run(endpoint="/404")
+        issues = self._get_issues(resp)
+        self._test_expecting_summary(
+            issues,
+            "bad",
+            "Page is 404 so should not be reachable in issues")
+
+        self._test_expecting_codes(
+            issues,
+            ["ALIVE-1"],
+            "Expecting to see site/page not reachable due to 404.")
+
+    """
+    def test_alive_timeout_return_not_reachable(self):
+        resp = self._run(timeout=True)
+        issues = self._get_issues(resp)
+        self._test_expecting_summary(
+            issues,
+            "bad",
+            "Site timeout so should not be reachable in issues")
+
+        self._test_expecting_codes(
+            issues,
+            ["ALIVE-1"],
+            "Expecting to see site/page not reachable due to timeout.")
+    """
